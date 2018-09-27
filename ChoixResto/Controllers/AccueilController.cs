541a45495f022f613171dded471e39ae60dfe3ec
Jonathan Halleux <jonathan.halleux@gmail.com>
2018-09-27 22:18:05 +0200

    Fin section View

---
+++ ChoixResto/Controllers/AccueilController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ChoixResto.Models;
+using ChoixResto.ViewModels;
+
+namespace ChoixResto.Controllers
+{
+    public class AccueilController : Controller
+    {
+        // GET: Accueil
+        public ActionResult Index()
+        {
+            AccueilViewModel vm = new AccueilViewModel
+            {
+                Message = "Bonjour depuis le <span style=\"color:red\">contrôleur</span>",
+
+                Date = DateTime.Now,
+                Restos = new Resto { Nom = "Resto pinambour", Telephone = "1234" }
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ ChoixResto/ViewModels/AccueilViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChoixResto.Models;
+
+namespace ChoixResto.ViewModels
+{
+    public class AccueilViewModel
+    {
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public Resto Restos { get; set; }
+    }
+}
